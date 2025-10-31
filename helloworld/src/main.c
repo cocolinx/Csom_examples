0e873b30d57cca2139e8ce35b9028f1a57d5e34b
cocolinx <cocolinx@cocolinx.com>
2025-10-31 16:00:04 +0900

    updated 10/31

--- helloworld/src/main.c
+++ helloworld/src/main.c
@@ -1,7 +1,5 @@
 #include <zephyr/kernel.h>
-#include <zephyr/logging/log.h>
-#include <nrf_modem.h> 
-#include <nrf_modem_at.h> 
+#include <zephyr/logging/log.h> 
 #include <modem/nrf_modem_lib.h>
 
 LOG_MODULE_REGISTER(main_helloworld, CONFIG_LOG_DEFAULT_LEVEL);
